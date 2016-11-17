9c0e301d2f33273d2bfbdca1cb302630d138678f
Natalie Antonova <5555482@gmail.com>
2016-11-17 21:19:59 +0000

    refactor auth login to separate class

--- Login.js
+++ Login.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var buffer = require('buffer');
 var React = require('react');
 var Component = require('react');
 var ReactNative = require('react-native');
@@ -16,8 +17,25 @@ var {
 class Login extends React.Component {
 	constructor(props){
 		super(props);
+		this.state = {
+			showProgress: false
+		}
 	}
 	render(){
+		var errorCtrl = <View />;
+
+		if(!this.state.success && this.state.badCredentials){
+			errorCtrl = <Text style={styles.error}>
+				That username and password combination did not work
+			</Text>;
+		}
+
+		if(!this.state.success && this.state.unknownError){
+			errorCtrl = <Text style={styles.error}>
+				We experienced an unexpected issue
+			</Text>;
+		}
+
 		return (
 			<View style={styles.container}>
 				<Image style={styles.logo}
@@ -43,11 +61,41 @@ class Login extends React.Component {
 						Log in
 					</Text>
 				</TouchableHighlight>
+				{errorCtrl}
 			</View>
 		);
 	}
 	onLoginPressed(){
 		console.log('Attempting to log in with username ' + this.state.username);
+
+		var b = new buffer.Buffer(this.state.username + ':' + this.state.password);
+		var encodedAuth = b.toString('base64');
+		
+		fetch('https://api.github.com/user',{
+			headers: {
+				'Authorization' : 'Basic ' + encodedAuth
+			}
+		})
+		.then((response)=> {
+			if(response.status >= 200 && response.status < 300){
+				return response;
+			}
+
+			throw {
+				badCredentials: response.status == 401,
+				unknownError: response.status != 401
+			}
+		})
+		.then((response)=> {
+			return response.json();
+		})
+		.then((results)=> {
+			console.log(results);
+			this.setState({success: true});
+		})
+		.catch((err)=> {
+			this.setState(err);
+		});
 	}
 }
 
@@ -86,6 +134,10 @@ var styles = StyleSheet.create({
   	fontSize: 22,
   	color: '#FFF',
   	alignSelf: 'center'
+  },
+  error: {
+  	color: 'red',
+  	paddingTop: 10
   }
 });
 module.exports = Login;
